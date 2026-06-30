486483682916ed296d52fc3db466def8cf773ddc
valen <valenbasiukcls@gmail.com>
2026-06-29 21:07:43 -0300

    eventloop mi comienzo

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
 use tracing::info;
 use tracing_subscriber;
+use calloop::EventLoop;
 
 fn main() {
     // sistema de logs
     tracing_subscriber::fmt::init();
-    
     info!("iniciando vinland...");
+
+
+    let mut event_loop: EventLoop<()> = EventLoop::try_new()
+        .expect("failed to initialize event loop");
+
+    info!("placeholder eventloop :3");
+
+    event_loop.run(None, &mut (), |_| {}).unwrap();
 }
