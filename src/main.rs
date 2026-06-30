ad608eff6c48972abc4b67758fbdfdfe377cd4a1
valen <valenbasiukcls@gmail.com>
2026-06-29 21:46:43 -0300

    display al loop

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,29 @@ fn main() {
     tracing_subscriber::fmt::init();
     info!("iniciando vinland...");
 
-    let mut display: Display<()> = Display::new().unwrap();
-    info!("display wayland creado :3");
-
     let mut event_loop: EventLoop<()> = EventLoop::try_new()
         .expect("failed to initialize event loop");
 
-    info!("placeholder eventloop :3");
+    let mut display: Display<()> = Display::new().unwrap();
 
+    let _display_handle = display.handle();
+    info!("display wayland creado");
+
+    let loop_handle = event_loop.handle();
+
+loop_handle
+    .insert_source(
+        smithay::reexports::calloop::generic::Generic::new(
+            display,
+            calloop::Interest::READ,
+            calloop::Mode::Level,
+        ),
+        |_, _, state| {
+            Ok(calloop::PostAction::Continue)
+        },
+    )
+    .unwrap();
+
+    info!("Display conectado al event loop");
     event_loop.run(None, &mut (), |_| {}).unwrap();
 }
