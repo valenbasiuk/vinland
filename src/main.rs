7347539034e9bfdcbc41551064301cb64565fd64
valen <valenbasiukcls@gmail.com>
2026-06-29 21:28:07 -0300

    wayland display

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 use tracing::info;
 use tracing_subscriber;
 use calloop::EventLoop;
+use smithay::reexports::wayland_server::Display;
 
 fn main() {
     // sistema de logs
     tracing_subscriber::fmt::init();
     info!("iniciando vinland...");
 
+    let mut display: Display<()> = Display::new().unwrap();
+    info!("display wayland creado :3");
 
     let mut event_loop: EventLoop<()> = EventLoop::try_new()
         .expect("failed to initialize event loop");
